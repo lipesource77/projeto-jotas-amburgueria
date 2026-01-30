f5e82540ff9a033577585320dc9689cb95c1be30
Carlos Santos <carlos.henrisantos708@gmail.com>
2026-01-30 10:40:53 -0300

    Apenas detalhes

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@
     spinner();
 
 
-    // Initiate the wowjs
+    // Iniciar o wowjs
     new WOW().init();
 
 
-    // Sticky Navbar
+    // Barra de navegação fixa
     $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
             $('.navbar').addClass('sticky-top shadow-sm');
@@ -25,36 +25,7 @@
         }
     });
 
-
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-
-    $(window).on("load resize", function () {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-                function () {
-                    const $this = $(this);
-                    $this.addClass(showClass);
-                    $this.find($dropdownToggle).attr("aria-expanded", "true");
-                    $this.find($dropdownMenu).addClass(showClass);
-                },
-                function () {
-                    const $this = $(this);
-                    $this.removeClass(showClass);
-                    $this.find($dropdownToggle).attr("aria-expanded", "false");
-                    $this.find($dropdownMenu).removeClass(showClass);
-                }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
-    });
-
-
-    // Back to top button
+    // Botão Voltar ao Topo
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
